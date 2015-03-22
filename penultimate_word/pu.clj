c849d815b5d83f4829905e371183098e294037c8
william <4yellowpeppers@gmail.com>
2015-03-22 12:49:12 -0400

    added intro comments

--- penultimate_word/pu.clj
+++ penultimate_word/pu.clj
@@ -1,3 +1,19 @@
+;; pu.clj
+;;
+;; By William Ersing
+;;
+;; This is a programming challenge from CodeEval.com.
+;; It reads in a file with each line containing a
+;; string of words. The goal is to locate the
+;; penultimate word (second from last) in each string
+;; and print it to stdout.
+;;
+;; Example:
+;;
+;;  "I loaf and lean at my ease"
+;;
+;;      The penultimate word is: "my"
+
 (require '[clojure.java.io :as io]
          '[clojure.string :as str])
 (with-open [rdr (io/reader (second *command-line-args*))]
--- penultimate_word/pu.hs
+++ penultimate_word/pu.hs
@@ -1,3 +1,20 @@
+{- pu.hs
+ -
+ - By William Ersing
+ -
+ - This is a programming challenge from CodeEval.com.
+ - It reads in a file with each line containing a
+ - string of words. The goal is to locate the
+ - penultimate word (second from last) in each string
+ - and print it to stdout.
+ -
+ - Example:
+ -
+ - 	"I loaf and lean at my ease"
+ -
+ - 		The penultimate word is: "my"
+ -}
+
 module Main where
 import System.Environment (getArgs)
 import Data.Char
