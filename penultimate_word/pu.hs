c564742eaa1089c241f329e8de785b30d7cb70c7
william <4yellowpeppers@gmail.com>
2015-03-27 09:37:44 -0400

    refactor finished... I think

--- penultimate_word/pu.hs
+++ penultimate_word/pu.hs
@@ -16,25 +16,20 @@
  -}
 
 import System.Environment (getArgs)
---import Data.Char
 
 main = do
 	inFile <- getArgs
 	file <- readFile $ head inFile
 	let linesList = lines file
 	mapM_ putStrLn $ penultimateWordsList linesList
-	--[inFile] <- getArgs
-	--file <- readFile inFile
-	--let linesList = lines file
-	--mapM_ putStrLn $ penultimateWordsList linesList
 
 -- yields the penultimate word in a string of words
 penultimateWord :: [String] -> String
 penultimateWord s
-	| length s == 0	= []
+	| null s		= []
 	| length s <= 2	= head s
 	| otherwise		= last $ init s
 
 -- yields list of penultimate words from list of strings
 penultimateWordsList :: [String] -> [String]
-penultimateWordsList ss = map penultimateWord $ map words ss
+penultimateWordsList = map (penultimateWord . words)
