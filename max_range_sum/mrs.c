5e6de0102bf3d9a7812a67c25d51dccf84212645
william <4yellowpeppers@gmail.com>
2015-03-27 10:51:16 -0400

    format testing

--- max_range_sum/mrs.c
+++ max_range_sum/mrs.c
@@ -40,88 +40,87 @@ int main(int argc, char *argv[]){
 
     // user entered correct number of args (i.e. 1)
     if(argc == 2){
-		FILE *fptr = fopen(argv[1], "r");
-
-		if(fptr == NULL){
-			printf("File not found, Exiting...\n");
-			exit(-1);
-		}
-		else{
-			char *periodptr, *spanptr, *spantoken;
-			long days[ARRAY_SIZE_MAX];	// gain/loss values for each day (D) in span (K)
-			long period;	// number of days over which 'max gains' is sought (N)
-			size_t buf_size = 10;
-			char *buf = malloc(buf_size);
-
-			while(getline(&buf, &buf_size, fptr) != -1){
-				size_t span_length = 0;
-
-				// temp buffer for strsep
-				spanptr = buf;
-
-				// grab up to ';' delim as string (input value for N)
-				periodptr = strsep(&spanptr, ";");
-
-				// convert period string to period long (N)
-				period = strtol(periodptr, &periodptr, 10);
-
-				// remove newline from span string (K)
-				spanptr = strsep(&spanptr, "\n");
-
-				// tokenize span (K) into days (D) - store in days[]
-				while((spantoken = strsep(&spanptr, " ")) != NULL){
-					days[span_length] = strtol(spantoken, &spantoken, 10);
-					span_length++;
-				}
-
-				// find highest grossing period (N) in the given span (K)
-				if(span_length >= period){
-					size_t i = 0, j = 0;
-					size_t k = period - 1;
-					long period_tl = days[j];
-					long period_hd = days[k];
-					long tmp_gains = 0, max_gains = 0;
-
-					for(i = 0; i < span_length; i++){
-						if(i <= k){
-							tmp_gains += days[i];
-						}
-						else{
-							tmp_gains -= days[j];
-							j++;
-							k++;
-							tmp_gains += days[k];
-						}
-						if(i == k && tmp_gains > max_gains){
-							max_gains = tmp_gains;
-						}
-					}
-
-					// print max gain for the given input
-					printf("%ld\n", max_gains);
-				}
-				else{
-					printf("Invalid Input:\tperiod (N) exceeds total days (K)\n");
-				}
-
-			}
-			fflush(stdout);
-			free(buf);
-		}
-		fclose(fptr);
-	}
-
-	// user entered too many args
-	else if(argc > 2){
-		printf("Too many arguments\n");
-		printf("Correct Usage: %s <path to file>\n", argv[0]);
-	}
-
-	// user entered too few args
-	else{
-		printf("Too few arguments\n");
-		printf("Correct Usage: %s <path to file>\n", argv[0]);
-	}
-
-	return 0;
+        FILE *fptr = fopen(argv[1], "r");
+        
+        if(fptr == NULL){
+            printf("File not found, Exiting...\n");
+            exit(-1);
+        }
+        else{
+            char *periodptr, *spanptr, *spantoken;
+            long days[ARRAY_SIZE_MAX];  // gain/loss values for each day (D) in span (K)
+            long period;                // number of days over which 'max gains' is sought (N)
+            size_t buf_size = 10;
+            char *buf = malloc(buf_size);
+            
+            while(getline(&buf, &buf_size, fptr) != -1){
+                size_t span_length = 0;
+                
+                // temp buffer for strsep
+                spanptr = buf;
+                
+                // grab up to ';' delim as string (input value for N)
+                periodptr = strsep(&spanptr, ";");
+                
+                // convert period string to period long (N)
+                period = strtol(periodptr, &periodptr, 10);
+                
+                // remove newline from span string (K)
+                spanptr = strsep(&spanptr, "\n");
+                
+                // tokenize span (K) into days (D) - store in days[]
+                while((spantoken = strsep(&spanptr, " ")) != NULL){
+                    days[span_length] = strtol(spantoken, &spantoken, 10);
+                    span_length++;
+                }
+                
+                // find highest grossing period (N) in the given span (K)
+                if(span_length >= period){
+                    size_t i = 0, j = 0;
+                    size_t k = period - 1;
+                    long period_tl = days[j];
+                    long period_hd = days[k];
+                    long tmp_gains = 0, max_gains = 0;
+                    
+                    for(i = 0; i < span_length; i++){
+                        if(i <= k){
+                            tmp_gains += days[i];
+                        }
+                        else{
+                            tmp_gains -= days[j];
+                            j++;
+                            k++;
+                            tmp_gains += days[k];
+                        }
+                        if(i == k && tmp_gains > max_gains){
+                            max_gains = tmp_gains;
+                        }
+                    }
+                    
+                    // print max gain for the given input
+                    printf("%ld\n", max_gains);
+                }
+                else{
+                    printf("Invalid Input:\tperiod (N) exceeds total days (K)\n");
+                }
+            }
+            fflush(stdout);
+            free(buf);
+        }
+        fclose(fptr);
+    }
+    
+    // user entered too many args
+    else if(argc > 2){
+        printf("Too many arguments\n");
+        printf("Correct Usage: %s <path to file>\n", argv[0]);
+    }
+    
+    // user entered too few args
+    else{
+        printf("Too few arguments\n");
+        printf("Correct Usage: %s <path to file>\n", argv[0]);
+    }
+    
+    return 0;
 }
