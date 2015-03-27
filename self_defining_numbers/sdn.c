fa664c99c2dbd54b69dc56b4b91ae93af691ce3a
william <4yellowpeppers@gmail.com>
2015-03-27 12:17:43 -0400

    formatting cleanup

--- self_defining_numbers/sdn.c
+++ self_defining_numbers/sdn.c
@@ -12,88 +12,88 @@
  * digit appears in the number.
  *
  * Example:
- * 		2020 is a Self Defining Number (sdn)
- * 			->	0 appears 2 times
- * 			->	1 appears 0 times
- * 			->	2 appears 2 times
- * 			->	3 appears 0 times
+ *      2020 is a Self Defining Number (sdn)
+ *          ->  0 appears 2 times
+ *          ->  1 appears 0 times
+ *          ->  2 appears 2 times
+ *          ->  3 appears 0 times
  */
 
 
-#include <stdlib.h>	// needed for calloc(), free(), exit()
-#include <stdio.h>	// needed for IO
-#include <string.h>	// needed for string manipulation
-#include <stdbool.h>	// needed for boolean
+#include <stdlib.h> // needed for calloc(), free(), exit()
+#include <stdio.h>  // needed for IO
+#include <string.h> // needed for string manipulation
+#include <stdbool.h>    // needed for boolean
 
 #define BUFSIZE 30
 
 int main(int argc, char *argv[]){
-	FILE *fptr;
-	char buf[BUFSIZE] = {0};
-	char digit_str[BUFSIZE] = {0};
-	int *digit_count;
+    FILE *fptr;
+    char buf[BUFSIZE] = {0};
+    char digit_str[BUFSIZE] = {0};
+    int *digit_count;
 
-	// user entered (correctly) one argument
-	if(argc == 2){
-		fptr = fopen(argv[1], "r+");
-		if(fptr == NULL){
-			printf("File does not exist. Exiting...\n");
-			exit(-1);
-		}
-		else{
-			while(fgets(buf, BUFSIZE, fptr) != NULL){
-				int digits = strlen(buf) - 1;
-				int is_sdn = false;
-				int i;
-				int max_digit = 0;
-				
-				// determine max digit
-				for(i = 0; i < digits; i++){
-					if((buf[i] - '0') > max_digit){
-						max_digit = buf[i] - '0';
-					}
-				}
+    // user entered (correctly) one argument
+    if(argc == 2){
+        fptr = fopen(argv[1], "r+");
+        if(fptr == NULL){
+            printf("File does not exist. Exiting...\n");
+            exit(-1);
+        }
+        else{
+            while(fgets(buf, BUFSIZE, fptr) != NULL){
+                int digits = strlen(buf) - 1;
+                int is_sdn = false;
+                int i;
+                int max_digit = 0;
+                
+                // determine max digit
+                for(i = 0; i < digits; i++){
+                    if((buf[i] - '0') > max_digit){
+                        max_digit = buf[i] - '0';
+                    }
+                }
 
-				if(digits >= max_digit){
-					digit_count = calloc(digits, sizeof(int));
-					for(i = 0; i < digits; i++){
-						int digit_val = buf[i] - '0';
-						digit_count[digit_val]++;
-					}
+                if(digits >= max_digit){
+                    digit_count = calloc(digits, sizeof(int));
+                    for(i = 0; i < digits; i++){
+                        int digit_val = buf[i] - '0';
+                        digit_count[digit_val]++;
+                    }
 
-					// convert int[] to char[]
-					memset(digit_str, 0, sizeof(digit_str));
-					for(i = 0; i < digits; i++){
-						 digit_str[i] = digit_count[i] + '0';
-					}
-					digit_str[digits] = '\n';
+                    // convert int[] to char[]
+                    memset(digit_str, 0, sizeof(digit_str));
+                    for(i = 0; i < digits; i++){
+                         digit_str[i] = digit_count[i] + '0';
+                    }
+                    digit_str[digits] = '\n';
 
-					// determine if digit count matches input number
-					if((strcmp(buf, digit_str)) == 0){
-						is_sdn = true;
-					}
+                    // determine if digit count matches input number
+                    if((strcmp(buf, digit_str)) == 0){
+                        is_sdn = true;
+                    }
 
-					free(digit_count);
-				}
+                    free(digit_count);
+                }
 
-				// output answers to stdout
-				printf("%d\n", is_sdn);
-			}
-		}
-		fclose(fptr);
-	}
+                // output answers to stdout
+                printf("%d\n", is_sdn);
+            }
+        }
+        fclose(fptr);
+    }
 
-	// Too many arguments provided by user
-	else if(argc > 2){
-		printf("Too many arguments\n");
-		printf("usage: %s <filename>\n", argv[0]);
-	}
+    // Too many arguments provided by user
+    else if(argc > 2){
+        printf("Too many arguments\n");
+        printf("usage: %s <filename>\n", argv[0]);
+    }
 
-	// Too few arguments provided by user
-	else{
-		printf("Too few arguments\n");
-		printf("usage: %s <filename>\n", argv[0]);
-	}
+    // Too few arguments provided by user
+    else{
+        printf("Too few arguments\n");
+        printf("usage: %s <filename>\n", argv[0]);
+    }
 
-	return 0;
+    return 0;
 }
