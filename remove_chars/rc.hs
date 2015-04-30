ae867ea9baa7b5e94b3f93316446c44ac457d6ed
william <wersing@willowbridge.com>
2015-04-30 10:25:05 -0400

    added header & comments

--- remove_chars/rc.hs
+++ remove_chars/rc.hs
@@ -1,3 +1,32 @@
+{- rc.hs
+ -
+ - By William Ersing
+ -
+ - This is a programming challenge from CodeEval.com.
+ - It reads in a file where each line contains two
+ - comma-separated fields. The first is a string, and
+ - the second is three characters. The program is to
+ - remove all instances of the characters (Field 2)
+ - from the string (Field 1), and print the resulting
+ - string.
+ -
+ - Example:
+ -
+ -      Input:  how are you, abc
+ -
+ -                  Field 1:    "how are you"
+ -                  Field 2:    "abc"
+ -
+ -      Output: how re you
+ -
+ - ======================================================
+ -
+ - TO DO:
+ -  ~ refactor rmCharsList so splitOn is only called once
+ -
+ - ======================================================
+ -}
+
 import System.Environment (getArgs)
 import Data.List.Split (splitOn)
 
@@ -8,9 +37,11 @@ main = do
     let lineslist = lines file
     mapM_ putStrLn $ rmCharsList lineslist
 
+-- remove selected characters from the string
 rmChars :: String -> String -> String
 rmChars = filter . flip notElem
 
+-- call rmChars on each line in list (input file)
 rmCharsList :: [String] -> [String]
 rmCharsList = map scrubList
     where
