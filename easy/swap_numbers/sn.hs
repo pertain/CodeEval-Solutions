a56d2b2c932428502fca0ff109079ad96bc8af72
william <wersing@willowbridge.com>
2015-06-02 10:30:27 -0400

    submitted version

--- easy/swap_numbers/sn.hs
+++ easy/swap_numbers/sn.hs
@@ -1,4 +1,31 @@
-import Data.List.Split (splitPlaces)
+{- sn.hs
+ -
+ - By William Ersing
+ -
+ - CodeEval challenge (Easy):   Swap Numbers
+ - https://www.codeeval.com/open_challenges/196/
+ -}
+
+import System.Environment (getArgs)
+import Data.List.Split (splitPlaces, splitOn)
 
 splitPrefixSuffix :: String -> [String]
 splitPrefixSuffix s = splitPlaces [1,length s - 2,1] s
+
+swapPrefixSuffix :: [String] -> String
+swapPrefixSuffix []         = []
+swapPrefixSuffix [x]        = x
+swapPrefixSuffix [x,y,z]    = z ++ y ++ x
+
+swapOneLine :: String -> String
+swapOneLine s = unwords $ map (swapPrefixSuffix . splitPrefixSuffix) (splitOn " " s)
+
+swapAllLines :: [String] -> [String]
+swapAllLines = map swapOneLine
+
+main :: IO ()
+main = do
+    inFile <- getArgs
+    file <- readFile $ head inFile
+    let linesList = lines file
+    mapM_ putStrLn $ swapAllLines linesList
