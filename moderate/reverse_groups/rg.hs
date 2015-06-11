355bebfe23f0ce21a6218a07c486412619eff9a8
william <wersing@willowbridge.com>
2015-06-11 16:26:02 -0400

    submitted but only partially correct

--- moderate/reverse_groups/rg.hs
+++ moderate/reverse_groups/rg.hs
@@ -1,9 +1,49 @@
-import qualified Data.List.Split as LS
+{- rg.hs
+ -
+ - By William Ersing
+ -
+ - (MODERATE) Reverse Groups
+ - https://www.codeeval.com/open_challenges/71/
+ -
+ - THIS HAS BEEN SUBMITTED, BUT ONLY RECIEVED PARTIAL CREDIT
+ - STILL WORKING TOWARD THE FULL SOLUTION
+ -}
 
---revGrps ss = foldr (\x acc -> if valid
+import System.Environment (getArgs)
+import Data.List (intersperse)
+import Data.List.Split (splitOneOf, splitEvery)
+import qualified Data.ByteString.Lazy.Char8 as LB
 
+main :: IO ()
+main = do
+    inFile <- getArgs
+    file <- readFile $ head inFile
+    let linesList = lines file
+    mapM_ putStrLn $ revAllLines linesList
 
-validLen ss = length (concat $ LS.splitOn "," ss) `mod` ss == 0
+bsToInt :: LB.ByteString -> Int
+bsToInt bs = case LB.readInt bs of
+    Nothing     -> error "Not and Integer"
+    Just (x,_)  -> x
 
+strToInt :: String -> Int
+strToInt = bsToInt . LB.pack
 
-revGrps ss = map reverse (LS.chunksOf 2 $ LS.splitOn "," ss)
+validMultiple :: [[String]] -> Int -> Bool
+validMultiple s k = length (concat s) `mod` k == 0
+
+revGrps :: [[String]] -> Int -> String
+revGrps sss k
+    | validMultiple sss k   = concat (concatMap reverse sss)
+    | otherwise             = concat (concatMap reverse (init sss) ++ last sss)
+
+finalizeLine :: String -> String
+finalizeLine s = intersperse ',' (revGrps grps k)
+    where
+        splitLn = splitOneOf ",;" s
+        k       = strToInt $ last splitLn
+        lst     = init splitLn
+        grps    = splitEvery k lst
+
+revAllLines :: [String] -> [String]
+revAllLines = map finalizeLine
