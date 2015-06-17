61a7487dcfcd0a43f1fb3246879f1da0c417b6df
william <4yellowpeppers@gmail.com>
2015-06-16 22:25:22 -0400

    submitted version

--- easy/mixed_content/mc.hs
+++ easy/mixed_content/mc.hs
@@ -1,16 +1,37 @@
-import Data.Char (isNumber)
+{- mc.hs
+ -
+ - By William Ersing
+ -
+ - CodeEval challenge (EASY) Mixed Content
+ - https://www.codeeval.com/open_challenges/115/
+ -}
+
+import System.Environment
+import Data.Char (isNumber, isLetter)
+import Data.List (intercalate)
 import Data.List.Split (splitOn)
 
-splitLn :: String -> [String]
-splitLn = splitOn ","
+main :: IO ()
+main = do
+    inFile <- getArgs
+    file <- readFile $ head inFile
+    let linesList = lines file
+    mapM_ putStrLn $ composeLines linesList
 
---organizeNums :: [String] -> String
-organizeNums = foldr (\x acc -> if isNumber (head x) then x : acc else acc) []
+composeNums :: [String] -> String
+composeNums = intercalate "," . foldr (\x acc -> if isNumber (head x) then x : acc else acc) []
 
---organizeStrings :: [String] -> String
-organizeStrings = foldr (\x acc -> if isNumber (head x) then acc else x : acc) []
+composeWords :: [String] -> String
+composeWords = intercalate "," . foldr (\x acc -> if isLetter (head x) then x : acc else acc) []
 
---organizeLine :: String -> String
-organizeLine s = organizeStrings x ++ organizeNums x
+handleLineType :: String -> String
+handleLineType s
+    | all isNumber y    = s
+    | all isLetter y    = s
+    | otherwise         = composeWords x ++ ('|' : composeNums x)
     where
         x = splitOn "," s
+        y = concat x
+
+composeLines :: [String] -> [String]
+composeLines = map handleLineType
