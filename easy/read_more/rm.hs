0c2ab68baf1bb3f089ccb150c63930da187ddde4
william <4yellowpeppers@gmail.com>
2015-06-19 07:51:44 -0400

    might be on my way to a solution

--- easy/read_more/rm.hs
+++ easy/read_more/rm.hs
@@ -21,7 +21,19 @@ main = do
     file <- readFile $ head inFile
     let linesList = lines file
     mapM_ putStrLn $ readMoreAll linesList
+    --putStrLn ""
 
+-- TO DO:   I think the problem comes from the trimTrunc portion.
+--          Right now, if the 40 length string ends with a space,
+--          then the space is trimmed. But, in doing this, the second
+--          part, trimSplit then cuts off another word, when it may
+--          only need to trim the last space. Change all this so the
+--          (possible) trailing space is only trimmed from strings
+--          that contain no spaces (i.e. length splitTrunc == 1), and
+--          rather than chopping off an entire word if length splitTrunc > 1,
+--          just have it trim the trailing space (if present). If no
+--          trailing space is present, then trim the last word as is
+--          done now.
 readMore :: String -> String
 readMore s
     | length s > 55 = rdMr
