3b59eceafd965e45ac9d06f651bf736ef18531ff
william <wersing@willowbridge.com>
2015-06-19 09:19:06 -0400

    submitted version - finally solved

--- easy/read_more/rm.hs
+++ easy/read_more/rm.hs
@@ -4,12 +4,6 @@
  -
  - CodeEval challenge (EASY) Read More
  - https://www.codeeval.com/open_challenges/167/
- -
- - =================================================
- - THIS WAS SUBMITTED BUT IS PARTIALLY CORRECT (92%)
- -
- -  ~ must continue to troubleshoot the problem
- - =================================================
  -}
 
 import System.Environment (getArgs)
@@ -21,33 +15,22 @@ main = do
     file <- readFile $ head inFile
     let linesList = lines file
     mapM_ putStrLn $ readMoreAll linesList
-    --putStrLn ""
 
--- TO DO:   I think the problem comes from the trimTrunc portion.
---          Right now, if the 40 length string ends with a space,
---          then the space is trimmed. But, in doing this, the second
---          part, trimSplit then cuts off another word, when it may
---          only need to trim the last space. Change all this so the
---          (possible) trailing space is only trimmed from strings
---          that contain no spaces (i.e. length splitTrunc == 1), and
---          rather than chopping off an entire word if length splitTrunc > 1,
---          just have it trim the trailing space (if present). If no
---          trailing space is present, then trim the last word as is
---          done now.
 readMore :: String -> String
 readMore s
     | length s > 55 = rdMr
     | otherwise     = s
     where
-        trunc                       = head $ splitPlaces [40] s
-        trimTrunc
-            | last trunc == ' '     = init trunc
-            | otherwise             = trunc
-        splitTrunc                  = words trimTrunc
-        trimSplit                   = unwords $ init splitTrunc
+        trunc       = head $ splitPlaces [40] s
+        truncLast   = last trunc
+        initTrunc   = init trunc
+        splitTrunc  = words trunc
+        trimSplit   = unwords $ init splitTrunc
         rdMr
-            | length splitTrunc > 1 = trimSplit ++ "... <Read More>"
-            | otherwise             = trimTrunc ++ "... <Read More>"
+            | length splitTrunc == 1    &&  truncLast == ' '    = initTrunc ++ "... <Read More>"
+            | length splitTrunc > 1     &&  truncLast == ' '    = initTrunc ++ "... <Read More>"
+            | length splitTrunc > 1     &&  truncLast /= ' '    = trimSplit ++ "... <Read More>"
+            | otherwise                                         = trunc ++ "... <Read More>"
 
 readMoreAll :: [String] -> [String]
 readMoreAll = map readMore
