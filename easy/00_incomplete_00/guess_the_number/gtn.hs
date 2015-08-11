dd428295ee3cc972a484a7e741ad46afd4f5ba06
william <4yellowpeppers@gmail.com>
2015-08-10 22:36:47 -0400

    submitted but only partially correct -- still working

--- easy/00_incomplete_00/guess_the_number/gtn.hs
+++ easy/00_incomplete_00/guess_the_number/gtn.hs
@@ -1,66 +1,28 @@
-import Data.List.Split (splitPlaces)
+{- gtn.hs
+ -
+ - By William Ersing
+ -
+ - CodeEval challenge (MODERATE) Guess the Number
+ - https://www.codeeval.com/open_challenges/170/
+ -}
 
-{-
-getHigherMiddle :: Int -> Int -> Int
-getHigherMiddle floor roof = floor + distance
-    where
-        distance    = round (fromIntegral (roof - floor) / 2)
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy.Char8 as LB
 
-getLowerMiddle :: Int -> Int -> Int
-getLowerMiddle floor roof = roof - distance
-    where
-        distance    = round (fromIntegral (roof - floor) / 2)
+main :: IO ()
+main = do
+    inFile <- getArgs
+    file <- readFile $ head inFile
+    let linesList = lines file
+    mapM_ putStrLn $ parseAllLines linesList
 
+bsToInt :: LB.ByteString -> Int
+bsToInt bs = case LB.readInt bs of
+    Nothing     -> error "Invalid Integer"
+    Just (x,_)  -> x
 
-getHiMid :: Int -> Int -> [Int]
-getHiMid floor roof = floor + distance : getHiMid newFloor roof
-    where
-        distance    = round (fromIntegral (roof - floor) / 2)
-        newFloor    = floor + distance + 1
-
-getLoMid :: Int -> Int -> [Int]
-getLoMid floor roof = roof - distance : getLoMid floor newRoof
-    where
-        distance    = round (fromIntegral (roof - floor) / 2)
-        newRoof     = roof - distance
-
-
--- this is the most recent portion -- continue testing here
-robusto :: String -> Int -> Int -> [Int]
-robusto hiLo floor roof
-    | hiLo == "Higher" && not (null $ head evenSplit) =
-        if even len
-        then head (last evenSplit) : robusto "Higher" newBound roof
-        else head (oddSplit !! 1) : robusto "Higher" newBound roof
-    | hiLo == "Lower" && not (null $ head evenSplit) =
-        if even len
-        then head (last evenSplit) : robusto "Lower" floor newBound
-        else head (oddSplit !! 1) : robusto "Lower" floor newBound
-    | otherwise = []
-    where
-        fullRange   = [floor .. roof]
-        len         = length fullRange
-        half        = len `div` 2
-        evenSplit   = splitPlaces [half, half] fullRange
-        oddSplit    = splitPlaces [half, 1, half] fullRange
-        newBound    = last $ head evenSplit
--}
-
--- UNDER CONSTRUCTION -- NOT QUITE THERE YET
-lowerMid :: Int -> Int -> Int
-lowerMid lo hi
-    | evenLen   = evenDist
-    | otherwise = oddDist
-    where
-        len         = length [lo .. hi]
-        evenLen     = even len
-        distance    = (hi - lo) `div` 2
-        evenDist    = roof - distance + 1
-        oddDist     = 
-
--- UNDER CONSTRUCTION -- NOT QUITE THERE YET
-higherMid :: Int -> Int -> Int
-higherMid lo hi
+getMid :: Int -> Int -> Int
+getMid lo hi
     | evenLen   = evenDist
     | otherwise = oddDist
     where
@@ -69,3 +31,29 @@ higherMid lo hi
         distance    = (hi - lo) `div` 2
         evenDist    = lo + distance + 1
         oddDist     = lo + distance
+
+getAllMids :: [String] -> Int -> Int -> [(Int,Int)]
+getAllMids hiLoLst initLo initHi
+    = foldr (\x acc ->
+                let oldLo   = fst $ head acc
+                    oldHi   = snd $ head acc
+                    olderHi = snd $ head $ tail acc
+                in
+                    if x == "Lower"
+                    then (oldLo, getMid oldLo (oldHi - 1)) : acc
+                    else (oldHi + 1, getMid oldHi (olderHi + 1)) : acc
+                    ) [(initLo, initHi)] hiLoLst
+
+getFinalNumLine :: [(Int,Int)] -> String
+getFinalNumLine = show . snd . head
+
+parseLine :: String -> String
+parseLine ls = getFinalNumLine $ getAllMids hiLoString floor roof
+    where
+        lineData    = init $ words ls
+        hiLoString  = reverse $ "Lower" : (tail lineData)
+        floor       = 0
+        roof        = bsToInt $ LB.pack $ head lineData
+
+parseAllLines :: [String] -> [String]
+parseAllLines = map parseLine
