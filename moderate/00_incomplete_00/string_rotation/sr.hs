a9501d3c2a4f5b55772e41080ea7febec07b08a0
william <wersing@willowbridge.com>
2015-08-12 16:42:02 -0400

    unsubmitted -- works partially -- need to handle repeated firstLetter

--- moderate/00_incomplete_00/string_rotation/sr.hs
+++ moderate/00_incomplete_00/string_rotation/sr.hs
@@ -1,10 +1,31 @@
+import System.Environment (getArgs)
 import Data.List.Split
 
+main :: IO ()
+main = do
+    inFile <- getArgs
+    file <- readFile $ head inFile
+    let linesList = lines file
+    mapM_ print $ parseAll linesList
+
+--parseLine :: String -> Bool
+--parseLine s = rotated == target
 parseLine :: String -> (String,String)
-parseLine s = (target,mergedRot)
+parseLine s = (target,rotated)
     where
         splitLine   = splitOn "," s
         scrambled   = last splitLine
         target      = head splitLine
-        firstLetter = head target
-        mergedRot   = concat $ split (keepDelimsL $ onSublist firstLetter) scrambled
+        firstLetter = take 1 target
+        splitScram  = split (keepDelimsL $ onSublist firstLetter) scrambled
+        --rotated     = last splitScram ++ head splitScram
+        rotated     = if length splitScram <= 2
+                      then last splitScram ++ head splitScram
+                      else last splitScram ++ concat (init splitScram)
+
+--parseAll :: [String] -> [Bool]
+--parseAll = map parseLine
+
+--parseAll :: [String] -> [Bool]
+parseAll :: [String] -> [(String,String)]
+parseAll = map parseLine
