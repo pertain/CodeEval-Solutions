40e773c8dc2d9324d7921a174147defee60c0b22
william <4yellowpeppers@gmail.com>
2015-08-22 11:24:48 -0400

    submitted version -- moved from incomplete to moderate dir

--- moderate/gronsfeld_cipher/gc.hs
+++ moderate/gronsfeld_cipher/gc.hs
@@ -1,3 +1,12 @@
+{- gc.hs
+ -
+ - By William Ersing
+ -
+ - CodeEval challenge (MODERATE) Gronsfeld Cipher
+ - https://www.codeeval.com/open_challenges/181/
+ -}
+
+import System.Environment (getArgs)
 import Data.List (dropWhile)
 import Data.List.Split (splitOn)
 import Data.Char (digitToInt, ord, chr)
@@ -7,20 +16,17 @@ import Data.Char (digitToInt, ord, chr)
 
 revGronsfeldKey :: String
 revGronsfeldKey =   "zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLK\
+                    \JIHGFEDCBA@?>=<:9876543210/.-,+*)('&%$#\"! \
+                    \zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLK\
                     \JIHGFEDCBA@?>=<:9876543210/.-,+*)('&%$#\"! "
 
---cipher :: String -> [Int]
---cipher s = map digitToInt si
-    --where
-        --splitLine   = splitOn ";" s
-        --si          = head splitLine
+main :: IO ()
+main = do
+    inFile <- getArgs
+    file <- readFile $ head inFile
+    let linesList = lines file
+    mapM_ putStrLn $ decipherAll linesList
 
-
---encipheredMsg :: String -> [String]
---encipheredMsg = tail . splitOn ";"
-
-
---charShiftPairs :: String -> [Int]
 charShiftPairs :: String -> [(Char,Int)]
 charShiftPairs s = zip encipheredMsg fullCipher
     where
@@ -40,3 +46,6 @@ decipherString :: String -> String
 decipherString s = map (uncurry decipherChar) pairs
     where
         pairs   = charShiftPairs s
+
+decipherAll :: [String] -> [String]
+decipherAll = map decipherString
