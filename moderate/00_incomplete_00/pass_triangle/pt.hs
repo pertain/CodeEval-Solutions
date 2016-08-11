be6a7f99c3009e9cbe5a61ede9df0eab33e182cd
William <4yellowpeppers@gmail.com>
2016-08-11 12:36:16 -0400

    working, and output matches pts.hs, but codeeval failed it as incorrect solution value

--- moderate/00_incomplete_00/pass_triangle/pt.hs
+++ moderate/00_incomplete_00/pass_triangle/pt.hs
@@ -7,22 +7,39 @@ main = do
     inFile <- getArgs
     file <- readFile $ head inFile
     let linesList = lines file
-    mapM_ putStrLn $ linesList
+    let ((i,pathVal):_) = getPath linesList
+    print pathVal
+    --mapM_ putStrLn $ linesList
+
+-------------------------------------------
 
 -- Non-monadic pass triangle challange
 mir :: Row -> Int -> (Int, Int)
 mir [] _    = (0, 0)
-mir [x] _   = (x, 0)
-
-mir [x, y] i
-    | x == max x y  = (x, i)
-    | otherwise     = (y, i + 1)
+mir [x] _   = (0, x)
 
 mir r i
-    | x' == max x' y'   = (x', i)
-    | otherwise         = (y', i + 1)
+    | x == max x y  = (i,x)
+    | otherwise     = (i+1,y)
     where
-        (x':y':_)   = drop i r
+        (x:y:_) = drop i r
 
-startState :: (Int, Int)
-startState = (0, 0)
+getPath :: [String] -> [(Int,Int)]
+getPath = foldl (\acc rw -> let
+                                row         = map read $ words rw
+                                ((i,t):_)   = acc
+                                (i',t')     = mir row i
+                            in
+                                (i',t+t') : acc
+                ) startVal
+
+--getPathVal :: [(Int,Int)] -> Int
+--getPathVal ((i,t):_) = 
+
+startVal :: [(Int, Int)]
+startVal = [(0, 0)]
+
+-------------------------------------------
+
+--startVal :: (Int, Int)
+--startVal = (0, 0)
